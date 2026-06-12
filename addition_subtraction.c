f98a00dab48340db2bc1f9e9c0403809be84d0e5
Antonis Mavrakis <a.mavrakis@proton.me>
2026-06-12 07:06:27 -0400

    added addition and subtraction basic only integers

---
+++ addition_subtraction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+{
+    int a;
+    int b;
+
+    printf("enter first number: ");
+    scanf("%d",&a);
+
+    printf("enter the second number: ");
+    scanf("%d",&b);
+
+
+    printf("addition: %d\n", a + b);
+    printf("subtraction: %d\n", a - b);
+
+    return 0;
+
+
+}
