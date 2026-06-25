89ddecebfb03c0ba678087fd163fa9b530b4658e
Antonis Mavrakis <a.mavrakis@proton.me>
2026-06-25 06:54:10 -0400

    simple function that sum 2 numbers

---
+++ simple_fuction.c
@@ -0,0 +1,17 @@
+/* creating a simple function add
+*/
+
+
+#include <stdio.h>
+
+int add(int a, int b) {
+	return a+b;
+}
+int main(void) {
+	int sum= add(10,90);
+	printf("sum: %d\n", sum);
+
+
+
+	return 0;
+}
